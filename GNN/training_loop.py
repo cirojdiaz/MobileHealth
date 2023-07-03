0f3a49623680f797ee32f001086ab08825f0a39f
James Chen <j.2002chen@gmail.com>
2023-07-03 18:35:14 -0400

    changed logic for early stopping to use val bal acc, finished LR scheduler implementation

--- GNN/training_loop.py
+++ GNN/training_loop.py
@@ -22,7 +22,7 @@ from xgboost import XGBClassifier
 
 
 # Look into optimal ESP 
-def train(g, node_batches, model, labels, train_mask, val_mask=[], test_mask=[], validate=True, test=False, init_lr=0.01, stoch=True, num_batches=20, early_stopping=True, early_stopping_patience=10, early_stopping_warmup=0, max_epochs=4950, lamb_beta1=0.9, lamb_beta2=0.999, lamb_eps=1e-6, lamb_wd=0.01, lookahead_k=5, lookahead_alpha=0.5):
+def train(g, node_batches, model, labels, train_mask, val_mask=[], test_mask=[], validate=True, test=False, init_lr=0.01, stoch=True, num_batches=20, early_stopping=True, early_stopping_patience=10, early_stopping_warmup=0, max_epochs=4950, lamb_beta1=0.9, lamb_beta2=0.999, lamb_eps=1e-6, lamb_wd=0.01, lookahead_k=5, lookahead_alpha=0.5, l1_lambda=0.01):
     # ensure that validation and/or test masks contain at least 1 node if validation and/or testing is enabled
     if test and len(test_mask) == 0:
         print("test_mask must contain at least 1 node if testing is enabled")
@@ -51,12 +51,14 @@ def train(g, node_batches, model, labels, train_mask, val_mask=[], test_mask=[],
 
     # Wrapper optimizer mantaining fast weights and slow weights updated every k steps
     optimizer = Lookahead(optimizer, k=lookahead_k, alpha=lookahead_alpha)
-    # Where should this be used?
+
     scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=15, verbose=True)
 
     # set best scores
     best_val_acc = 0
     best_test_acc = 0
+    best_val_bal_acc = 0
+    best_scores = {}
 
     # extract node and edge features from graph object
     features = g.ndata["feat"]
@@ -71,6 +73,7 @@ def train(g, node_batches, model, labels, train_mask, val_mask=[], test_mask=[],
     #class_weights_weighted = class_weights * class_weights_multiplier
     
     prev_val_loss = 0
+    prev_bal_val_acc = 0
     bad_epoch_cnt = 0
 
     #logit_multiplier = torch.tensor([1, 1])
@@ -88,6 +91,13 @@ def train(g, node_batches, model, labels, train_mask, val_mask=[], test_mask=[],
 
         # Inputs: logits, target:labels
         loss = loss_function(logits[train_mask].squeeze(), labels[train_mask].squeeze().float())
+
+        # L1 regularization
+        l1_lambda = 0.01
+        for p in model.parameters():
+            if p.dim() > 1:
+                loss += l1_lambda * p.abs().sum()
+
         # Compute loss
         # Note that you should only compute the losses of the nodes in the current training set batch. <- don't worry about this note
         #print(class_weights_weighted.float())
@@ -97,7 +107,6 @@ def train(g, node_batches, model, labels, train_mask, val_mask=[], test_mask=[],
         # Clear gradients of optimized variables before backward pass
         optimizer.zero_grad()
         loss.backward()
-        # scheduler.step(F.cross_entropy(logits[train_mask], labels[train_mask], weight=class_weights_weighted))
         optimizer.step()
 
         # Compute accuracy on entire training/validation/test
@@ -107,6 +116,8 @@ def train(g, node_batches, model, labels, train_mask, val_mask=[], test_mask=[],
         train_bal_acc = metrics.balanced_accuracy_score(labels[train_mask], pred[train_mask])
         train_recall = metrics.recall_score(labels[train_mask], pred[train_mask])
         train_precision = metrics.precision_score(labels[train_mask], pred[train_mask], zero_division=0)
+        train_f1 = metrics.f1_score(labels[train_mask], pred[train_mask])
+        train_auc = metrics.roc_auc_score(labels[train_mask], pred[train_mask])
 
         if validate:
             val_acc = (pred[val_mask] == labels[val_mask]).float().mean()
@@ -117,18 +128,45 @@ def train(g, node_batches, model, labels, train_mask, val_mask=[], test_mask=[],
             val_precision = metrics.precision_score(labels[val_mask], pred[val_mask], zero_division=0)
             #val_loss = F.cross_entropy(logits[val_mask], labels[val_mask].reshape(-1).long(), weight=class_weights_weighted.float())
             val_loss = loss_function(logits[val_mask].squeeze().float(), labels[val_mask].squeeze().float())
+            val_f1 = metrics.f1_score(labels[val_mask], pred[val_mask])
+            val_auc = metrics.roc_auc_score(labels[val_mask], pred[val_mask])
 
+            # Learning rate scheduling
+            scheduler.step(val_loss.item())
             # early stopping, we will not worry about this for now
             if e == 0:
-                prev_val_loss = val_loss
-            if val_loss > prev_val_loss and e >= early_stopping_warmup:
+                prev_bal_val_acc = bal_acc
+            if val_acc > prev_bal_val_acc and e >= early_stopping_warmup:
                 bad_epoch_cnt += 1
             else:
                 bad_epoch_cnt = 0
-            prev_val_loss = val_loss
+            prev_bal_val_acc = bal_acc
             if bad_epoch_cnt > early_stopping_patience:
                 break
             
+        # Save the best validation accuracy and the corresponding test accuracy.
+            if best_val_acc < bal_acc and e > 0:
+                best_val_acc = val_acc
+                best_val_auc = val_auc
+                best_val_bal_acc = bal_acc
+                if test:
+                    best_test_acc = test_acc
+                best_scores = {'epoch': e,
+                                'train_loss': train_loss.item(),
+                                'train_acc': train_acc.item(),  
+                                'train_bal_acc': train_bal_acc,
+                                'train_recall': train_recall,
+                                'train_precision': train_precision,
+                                'train_f1': train_f1,
+                                'train_auc': train_auc,
+                                'val_loss': val_loss.item(),
+                                'val_acc': val_acc.item(), 
+                                'val_bal_acc': bal_acc,
+                                'val_recall': val_recall,
+                                'val_precision': val_precision,
+                                'val_f1': val_f1,
+                                'val_auc': val_auc}
+
         if test:
             test_acc = (pred[test_mask] == labels[test_mask]).float().mean()
             fpr, tpr, thresholds = metrics.roc_curve(labels[test_mask], pred[test_mask], pos_label=1)
@@ -138,15 +176,12 @@ def train(g, node_batches, model, labels, train_mask, val_mask=[], test_mask=[],
             test_precision = metrics.precision_score(labels[test_mask], pred[test_mask], zero_division=0)
             #test_loss = F.cross_entropy(logits[test_mask], labels[test_mask].reshape(-1).long(), weight=class_weights_weighted.reshape(-1).float())
             test_loss = loss_function(logits[test_mask].squeeze().float(), labels[test_mask].squeeze().float())
-        # Save the best validation accuracy and the corresponding test accuracy.
-        if validate and best_val_acc < val_acc:
-            best_val_acc = val_acc
-            best_val_auc = val_auc
-            best_bal_acc = bal_acc
-            if test:
-                best_test_acc = test_acc
+            test_f1 = metrics.f1_score(labels[test_mask], pred[test_mask])
+            test_auc = metrics.roc_auc_score(labels[test_mask], pred[test_mask])
+
 
         # print out results every 5 epochs
+        '''
         if e % 5 == 0:
             print(
                 "In epoch {}, loss: {:.3f}, train_acc: {:.3f}, train_bal_acc: {:.3f}, train_recall: {:.3f}, train_precision: {:.3f}".format(
@@ -167,7 +202,7 @@ def train(g, node_batches, model, labels, train_mask, val_mask=[], test_mask=[],
                     ), end=""
                 )
             print("")
-
-
-
-    return best_val_acc
+        '''
+    
+    print("Best scores in iteration: ", best_scores)
+    return best_val_bal_acc
